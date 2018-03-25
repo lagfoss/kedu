0f13e8f5afe355b5bd7b98d80f2268564b3c8cb5
lagfoss <32448248+lagfoss@users.noreply.github.com>
2018-03-25 23:00:42 +0200

    login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 // console.log("YAY!"); for debugging use
+function getInfo() {
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+  console.log("")
+
+}
