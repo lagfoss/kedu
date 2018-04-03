d89bda7c0f718a0f96f5d3dafd333c32c92767f0
lagfoss <32448248+lagfoss@users.noreply.github.com>
2018-04-03 09:07:35 +0200

    JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,10 @@ function getInfo() {
 
 
 /*Manuals*/
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
+function myFunction(a) {
+  a.parentNode.getElementsByClassName("dropmanu-content")[0].classList.toggle("show");
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.manu')) {
