e447628e60a1cc16b8eef56c215dcd4b4825a5a9
lagfoss <32448248+lagfoss@users.noreply.github.com>
2018-04-10 16:18:23 +0200

    seperate js files

---
+++ assets/js/login.js
@@ -0,0 +1,20 @@
+//console.log("YAY!"); for debugging use
+function getInfo() {
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+  console.log("")
+}
+
+document.getElementById("loginButton").onclick = function () {
+  location.href = "dashb.html";
+}
+
+document.getElementById("signup").onclick = function () {
+  location.href = "regt.html";
+}
+
+/*if.getElementById("newsite").innerHTML=""
+document.getElementById("successbox").style.display = "block";
+if(email ==""){
+ document.getElementById("email").style.border ="2px solid red";
+}
